9978ccb8ac915039e72a0ff93e7f3f9e436f3c07
Felix Bechstein <felix.bechstein.extern@vtg.com>
2022-07-04 09:13:30 +0200

    CE-209 explicitly set aws managed keys

--- modules/glue-baseline/main.tf
+++ modules/glue-baseline/main.tf
@@ -1,18 +1,17 @@
+data "aws_kms_alias" "glue" {
+  name = "alias/aws/glue"
+}
+
 resource "aws_glue_data_catalog_encryption_settings" "this" {
   data_catalog_encryption_settings {
     connection_password_encryption {
       return_connection_password_encrypted = true
+      aws_kms_key_id                       = data.aws_kms_alias.glue.arn
     }
 
     encryption_at_rest {
       catalog_encryption_mode = "SSE-KMS"
+      sse_aws_kms_key_id      = data.aws_kms_alias.glue.arn
     }
-  }
-
-  lifecycle {
-    ignore_changes = [
-      data_catalog_encryption_settings[0].connection_password_encryption[0].aws_kms_key_id,
-      data_catalog_encryption_settings[0].encryption_at_rest[0].sse_aws_kms_key_id,
-    ]
   }
 }
